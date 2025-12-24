c3823485bb388e6e7b87844d937d1cbf5defd715
Sakol Bun <112042178+SakolB@users.noreply.github.com>
2025-12-23 19:38:08 -0800

    added main.go integrating crawler, ranker, and TUI together

--- src/main.go
+++ src/main.go
@@ -2,20 +2,57 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/sakolb/bcd/src/crawler"
+	"github.com/sakolb/bcd/src/entry"
+	"github.com/sakolb/bcd/src/tui"
 )
 
 func main() {
-	c := crawler.NewCrawler()
-	pathChan := c.Paths()
-	go c.Crawl("./")
-	go func() {
-		for err := range c.Errors() {
-			fmt.Println(err)
+	baseDir, err := os.Getwd()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+	if len(os.Args) > 1 {
+		baseDir, err = filepath.Abs(os.Args[1])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error: %v\n", err)
+			os.Exit(1)
 		}
+	}
+
+	model := tui.InitModel(baseDir)
+
+	p := tea.NewProgram(&model, tea.WithAltScreen())
+
+	c := crawler.NewCrawler()
+	go func() {
+		go c.Crawl(baseDir)
+
+		for path := range c.Paths() {
+			e, err := entry.NewPathEntry(path, baseDir)
+			if err != nil {
+				continue
+			}
+			p.Send(tui.EntryMsg(e))
+		}
+		p.Send(tui.CrawlDoneMsg{})
 	}()
-	for path := range pathChan {
-		fmt.Println(path)
+
+	finalModel, err := p.Run()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+
+	if m, ok := finalModel.(*tui.Model); ok {
+		fmt.Println(ok)
+		if selected := m.Selected(); selected != "" {
+			fmt.Println(selected)
+		}
 	}
 }
